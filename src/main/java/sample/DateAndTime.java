67f9415743925efec8da924adfe05d8f8ce2387b
akademiasovy <roland.onofrej@akademiasovy.sk>
2020-04-29 11:00:14 +0200

    added User class

--- src/main/java/sample/DateAndTime.java
+++ src/main/java/sample/DateAndTime.java
@@ -1,12 +1,17 @@
 package sample;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class DateAndTime {
+    List<User> list =  new ArrayList<User>();
+
+    public DateAndTime() {
+        list.add(new User("Roman","Simko","roman","heslo"));
+    }
 
     @RequestMapping("/time")
     public String getTime() {
@@ -36,6 +41,19 @@ public class DateAndTime {
     @RequestMapping("/hi")
     public String getHi(@RequestParam(value="fname") String fname, @RequestParam(value="age") String age){
         return "Hello. How are you? Your name is "+fname+" and you are "+age;
+    }
+
+    @RequestMapping(method=RequestMethod.POST, value="/login")
+    public String login(@RequestBody String credential){
+        System.out.println(credential);
+        return "{\"Error\":\"Login already exists\"}";
+    }
+
+    @RequestMapping(method=RequestMethod.POST, value="/signup")
+    public String signup(@RequestBody String data){
+        System.out.println(data);
+
+        return data;
     }
 
 }
